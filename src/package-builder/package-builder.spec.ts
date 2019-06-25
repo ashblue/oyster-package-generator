ac7493763955e5ea7fe3dcc6fc9a78eafee22af2
Ash Blue <ash@blueashes.com>
2019-06-24 20:49:46 -0600

    Started creation of PackageBuilder class (partially complete)

---
+++ src/package-builder/package-builder.spec.ts
@@ -0,0 +1,9 @@
+describe('PackageBuilder', () => {
+  describe('Build method', () => {
+    it('should give the copy folder source as src/templates/Assets');
+
+    it('should give the copy folder destination of Assets');
+
+    it('should send terminal answers as variable replacement option');
+  });
+});
