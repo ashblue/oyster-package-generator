870d2a9e01668494035d002120d5fe15d4cc0534
Ash Blue <ash@blueashes.com>
2019-06-27 17:32:40 -0600

    Fixed a bug where running outside the package root lost the template location

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const gitRemoteOriginUrl = require('git-remote-origin-url');
 
 import * as inquirer from 'inquirer';
+import * as path from 'path';
 import {copyFolder} from './copy-folder/copy-folder';
 import {GitDetector} from './git-detector/git-detector';
 import {PackageBuilder} from './package-builder/package-builder';
@@ -10,10 +11,11 @@ async function runBuild() {
   const terminal = new Terminal(inquirer);
   const gitDetector = new GitDetector(gitRemoteOriginUrl);
   const packageBuilder = new PackageBuilder(copyFolder, terminal, gitDetector);
+
   return packageBuilder.Build([
     {
       destination: 'Assets',
-      source: 'src/templates/assets',
+      source: path.resolve(__dirname, '../src/templates/assets'),
     },
   ]);
 }
