ef3c37d0d60a031d617a4fb000f18a8646b4331d
Ash Blue <ash@blueashes.com>
2021-03-25 20:30:24 -0600

    ci(tests): suite now monitors code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 module.exports = {
   clearMocks: true,
+  modulePathIgnorePatterns: [
+    'src/templates',
+  ],
+  collectCoverageFrom: [
+    'src/**/*',
+  ],
   coverageDirectory: "dist/coverage",
+  coverageThreshold: {
+    global: {
+      branches: 78,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    }
+  },
   moduleFileExtensions: [
     "js",
     "ts",
-    "tsx"
   ],
   testEnvironment: "node",
   testMatch: [
